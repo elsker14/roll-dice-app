762492b3444ebecb7ae6d5f8116a2fbc02607e7f
Iancu Jianu <iancujianu98@gmail.com>
2025-03-22 14:53:16 +0200

    removed redundant unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,2 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:roll_dice_app/main.dart';
-
 void main() {
-  testWidgets('Verify text elements in MyApp', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that specific text elements exist.
-    expect(find.text('Hello World!!'), findsOneWidget);
-    expect(find.text('It\'s time to learn Flutter!'), findsOneWidget);
-
-    // Verify that there's no unexpected text.
-    expect(find.text('Counter'), findsNothing);
-  });
 }
